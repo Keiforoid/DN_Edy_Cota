09f91945edc1579cb9f918f118bd6c0f16079b0a
Edy Jesús Manuel Cota Villa <75243865+Keiforoid@users.noreply.github.com>
2021-12-04 12:49:35 -0800

    02_NetProjects_T2_Finished

---
+++ 03_NetProjects/T2/GymManager.Web/GymManager.ApplicationServices/Members/MembersAppService.cs
@@ -0,0 +1,113 @@
+﻿using GymManager.Core.Members;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManager.ApplicationServices.Members
+{
+    public class MembersAppService : IMemberAppService
+    {
+        List<Member> IMemberAppService.GetMembers()
+        {
+            List<Member> members = new List<Member>();
+
+            // 1
+            members.Add(new Member
+            {
+                Name = "Israel",
+                LastName = "Pérez",
+                BirthDay = new DateTime(1986, 6, 24),
+                AllowNewsLetter = true,
+                CityId = 1,
+                Email = "isra@prueba.com"
+            });
+            // 2
+            members.Add(new Member
+            {
+                Name = "Edgar",
+                LastName = "García",
+                BirthDay = new DateTime(2000, 10, 11),
+                AllowNewsLetter = false,
+                CityId = 1,
+                Email = "edgar@prueba.com"
+            });
+            // 3
+            members.Add(new Member
+            {
+                Name = "Jaime",
+                LastName = "Castillo",
+                BirthDay = new DateTime(1981, 9, 21),
+                AllowNewsLetter = true,
+                CityId = 1,
+                Email = "jaime@prueba.com"
+            });
+            members.Add(new Member
+            {
+                Name = "Juan",
+                LastName = "López",
+                BirthDay = new DateTime(1966, 9, 4),
+                AllowNewsLetter = true,
+                CityId = 1,
+                Email = "juan@prueba.com"
+            });
+            members.Add(new Member
+            {
+                Name = "Elon",
+                LastName = "Musk",
+                BirthDay = new DateTime(2000, 12, 2),
+                AllowNewsLetter = true,
+                CityId = 1,
+                Email = "isra@prueba.com"
+            });
+            members.Add(new Member
+            {
+                Name = "Bart",
+                LastName = "Simpson",
+                BirthDay = new DateTime(1982, 4, 20),
+                AllowNewsLetter = true,
+                CityId = 3,
+                Email = "bart@prueba.com"
+            });
+            members.Add(new Member
+            {
+                Name = "Homero",
+                LastName = "Simpson",
+                BirthDay = new DateTime(1966, 2, 22),
+                AllowNewsLetter = true,
+                CityId = 3,
+                Email = "homer@prueba.com"
+            });
+            members.Add(new Member
+            {
+                Name = "Rick",
+                LastName = "Sanchez",
+                BirthDay = new DateTime(1960, 2, 10),
+                AllowNewsLetter = true,
+                CityId = 6,
+                Email = "rick@prueba.com"
+            });
+            members.Add(new Member
+            {
+                Name = "Summer",
+                LastName = "Smith",
+                BirthDay = new DateTime(2003, 1, 3),
+                AllowNewsLetter = true,
+                CityId = 6,
+                Email = "isra@prueba.com"
+            });
+            members.Add(new Member
+            {
+                Name = "Morty",
+                LastName = "Smith",
+                BirthDay = new DateTime(2008, 5, 11),
+                AllowNewsLetter = true,
+                CityId = 6,
+                Email = "morty@prueba.com"
+            });
+
+            return members;
+        }
+    }
+}
---
+++ 03_NetProjects/T2/GymManager.Web/GymManager.Web/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using GymManager.ApplicationServices.Members;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymManager.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IMemberAppService _memberAppService;
+        public HomeController(IMemberAppService memberAppService)
+        {
+            _memberAppService = memberAppService;
+        }
+        public IActionResult Index()
+        {
+            var members = _memberAppService.GetMembers();
+
+            return View(members);
+        }
+    }
+}
