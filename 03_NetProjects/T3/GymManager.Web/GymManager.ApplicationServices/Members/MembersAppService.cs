27a6f609b6ca3d3e2e8a6412cdc16c2fb2c8d7e0
Edy Jesús Manuel Cota Villa <75243865+Keiforoid@users.noreply.github.com>
2021-12-04 20:17:47 -0800

    02_NetProjects_T3_Finished

---
+++ 03_NetProjects/T3/GymManager.Web/GymManager.ApplicationServices/Members/MembersAppService.cs
@@ -0,0 +1,27 @@
+﻿using GymManager.Core.Members;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManager.ApplicationServices.Members
+{
+    public class MembersAppService : IMemberAppService
+    {
+        private static List<Member> Members = new List<Member>();
+
+        int IMemberAppService.AddMember(Member member)
+        {
+            member.Id = new Random().Next();
+            Members.Add(member);
+
+            return member.Id;
+        }
+
+        List<Member> IMemberAppService.GetMembers()
+        {
+            return Members;
+        }
+    }
+}
---
+++ 03_NetProjects/T3/GymManager.Web/GymManager.Web/Controllers/MembersController.cs
@@ -0,0 +1,51 @@
+﻿using GymManager.ApplicationServices.Members;
+using GymManager.Core.Members;
+using GymManager.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymManager.Web.Controllers
+{
+    public class MembersController : Controller
+    {
+        private readonly IMemberAppService _memberAppService;
+        public MembersController(IMemberAppService memberAppService)
+        {
+            _memberAppService = memberAppService;
+        }
+
+        public IActionResult Index()
+        {
+            List<Member> members = _memberAppService.GetMembers();
+
+            MemberListViewModel viewModel = new MemberListViewModel();
+
+            viewModel.NewMembersCount = 2;
+            viewModel.Members = members;
+
+            return View(viewModel);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Member member)
+        {
+            _memberAppService.AddMember(member);
+
+
+            return  RedirectToAction("Index");
+        }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+    }
+}
