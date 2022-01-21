6db3d030de5c8072c9679f4d68fbb00be37831f4
Edy Jesús Manuel Cota Villa <75243865+Keiforoid@users.noreply.github.com>
2022-01-20 20:27:16 -0800

    06_Entity_Framework_T7_Finished

---
+++ 06_AspNetCore_MVC_con_Entity_Framework/T7/GymManager.Web/GymManager.ApplicationServices/MembershipTypes/MembershipTypeAppService.cs
@@ -0,0 +1,55 @@
+﻿using GymManager.Core.Members;
+using GymManager.Core.MembershipTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManager.ApplicationServices.MembershipTypes
+{
+    public class MembershipTypeAppService : IMembershipTypeAppService
+    {
+        private static List<Member> Members = new List<Member>();
+
+        private static List<MembershipType> membershipTypes = new List<MembershipType>();
+
+        public List<MembershipType> GetMembershipTypes()
+        {
+            return membershipTypes;
+        }
+
+        public MembershipType GetMembershipType(int membershipTypeId)
+        {
+            var m = membershipTypes.Where(x => x.Id == membershipTypeId).FirstOrDefault();
+
+            return m;
+        }
+
+        public int AddMembershipType(MembershipType membershipType)
+        {
+            membershipType.Id = new Random().Next();
+            membershipType.CreatedOn = DateTime.Now;
+            membershipTypes.Add(membershipType);
+
+            return membershipType.Id;
+        }
+
+        public void EditMembershipType(MembershipType membershipType)
+        {
+            var m = membershipTypes.Where(x => x.Id == membershipType.Id).FirstOrDefault();
+
+            m.Name = membershipType.Name;
+            m.Cost = membershipType.Cost;
+            m.CreatedOn = membershipType.CreatedOn;
+            m.Duration = membershipType.Duration;
+        }
+
+        public void DeleteMembershipType(int membershipTypeId)
+        {
+            var m = membershipTypes.Where(x => x.Id == membershipTypeId).FirstOrDefault();
+
+            membershipTypes.Remove(m);
+        }
+    }
+}
---
+++ 06_AspNetCore_MVC_con_Entity_Framework/T7/GymManager.Web/GymManager.ApplicationServices/Navigation/MenuAppService.cs
@@ -0,0 +1,216 @@
+﻿using GymManager.Core.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManager.ApplicationServices.Navigation
+{
+    public class MenuAppService : IMenuAppService
+    {
+        public List<UserMenuitem> GetMenu()
+        {
+            List<UserMenuitem> menu = new List<UserMenuitem>();
+            
+            menu.Add(new UserMenuitem
+            {
+                Name = "Home",
+                DisplayName = "Home",
+                Order = 0,
+                Url = "/"
+            });
+
+            menu.Add(new UserMenuitem
+            {
+                Name = "Administration",
+                DisplayName = "Administration",
+                Order = 1,
+                Url = "#",
+                Items = new List<UserMenuitem>()
+                {
+                    new UserMenuitem
+                    {
+                        Name = "MembershipTypes",
+                        DisplayName = "Membership Types",
+                        Order = 0,
+                        Url = "/MembershipTypes/"
+                    },
+                    new UserMenuitem
+                    {
+                        Name = "InventoryUnits",
+                        DisplayName = "Inventory Units",
+                        Order = 1,
+                        Url = "/MembershipTypes/"
+                    },
+                    new UserMenuitem
+                    {
+                        Name = "EquipmentTypes",
+                        DisplayName = "Equipment Types",
+                        Order = 2,
+                        Url = "/EquipmentTypes/"
+                    },
+                    new UserMenuitem
+                    {
+                        Name = "EquipmentInventory",
+                        DisplayName = "Equipment Inventory",
+                        Order = 3,
+                        Url = "/EquipmentInventory/"
+                    },
+                    new UserMenuitem
+                    {
+                        Name = "UserManagment",
+                        DisplayName = "Users",
+                        Order = 4,
+                        Url = "/Users/"
+                    }
+                }
+            });
+
+            menu.Add(new UserMenuitem
+            {
+                Name = "Store",
+                DisplayName = "Store",
+                Order = 2,
+                Url = "#",
+                Items = new List<UserMenuitem>
+                { 
+                    new UserMenuitem
+                    {
+                        Name = "Shopping",
+                        DisplayName = "Shopping",
+                        Order = 0,
+                        Url = "/Shopping/",
+                    },
+                    new UserMenuitem
+                    {
+                        Name = "ProductInventory",
+                        DisplayName = "Product Inventory",
+                        Order = 1,
+                        Url = "/ProductInventory/",
+                    },
+                    new UserMenuitem
+                    {
+                        Name = "ProducTypes",
+                        DisplayName = "Produc Types",
+                        Order = 2,
+                        Url = "/ProducTypes/",
+                    },
+                    new UserMenuitem
+                    {
+                        Name = "ProductCategories",
+                        DisplayName = "Product Categories",
+                        Order = 3,
+                        Url = "/ProductCategories/",
+                    }
+                }
+            });
+
+            menu.Add(new UserMenuitem
+            {
+                Name = "Members",
+                DisplayName = "Members",
+                Order = 3,
+                Url = "#",
+                Items = new List<UserMenuitem>()
+                {
+                    new UserMenuitem
+                    {
+                        Name = "MemberManager",
+                        DisplayName = "Managment",
+                        Order = 0,
+                        Url = "/Members/"
+                    },
+                    new UserMenuitem
+                    {
+                        Name = "MembershipRenewal",
+                        DisplayName = "Membership Renewal",
+                        Order = 1,
+                        Url = "/Membership/Renewal/"
+                    },
+                    new UserMenuitem
+                    {
+                        Name = "CheckIn",
+                        DisplayName = "CheckIn",
+                        Order = 2,
+                        Url = "/Members/CheckIn/"
+                    },
+                    new UserMenuitem
+                    {
+                        Name = "CheckOut",
+                        DisplayName = "CheckOut",
+                        Order = 3,
+                        Url = "/Members/CheckOut/"
+                    }
+                }
+            });
+
+            menu.Add(new UserMenuitem
+            {
+                Name = "Invoicing",
+                DisplayName = "Invoicing",
+                Order = 4,
+                Url = "/Invoicing"
+            });
+
+            menu.Add(new UserMenuitem
+            {
+                Name = "Reports",
+                DisplayName = "Reports",
+                Order = 5,
+                Url = "#",
+                Items = new List<UserMenuitem>()
+                {
+                    new UserMenuitem
+                    {
+                        Name = "NewMembersReport",
+                        DisplayName = "New Members",
+                        Order = 0,
+                        Url = "/Reports/NewMembers/"
+                    },
+                    new UserMenuitem
+                    {
+                        Name = "AttendanceReport",
+                        DisplayName = "Attendance",
+                        Order = 1,
+                        Url = "/Reports/Attendance/"
+                    },
+                    new UserMenuitem
+                    {
+                        Name = "MembershipRenewalReport",
+                        DisplayName = "Membership Renewal",
+                        Order = 2,
+                        Url = "/Reports/MembershipRenewal/"
+                    }
+                }
+            });
+
+            menu.Add(new UserMenuitem
+            {
+                Name = "Attendance",
+                DisplayName = "Attendance",
+                Order = 6,
+                Url = "#",
+                Items = new List<UserMenuitem>()
+                {
+                    new UserMenuitem
+                    {
+                        Name = "MemberIn",
+                        DisplayName = "Member In",
+                        Order = 0,
+                        Url = "/Attendance/MemberIn/"
+                    },
+                    new UserMenuitem
+                    {
+                        Name = "MemberOut",
+                        DisplayName = "Member Out",
+                        Order = 1,
+                        Url = "/Attendance/MemberOut/"
+                    },
+                }
+            });
+
+            return menu;
+        }
+    }
+}
