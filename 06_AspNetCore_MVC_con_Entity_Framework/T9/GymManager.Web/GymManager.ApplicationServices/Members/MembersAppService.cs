77eb2bede45381ccbf8bd1c4228c2390360ae572
Edy Jesús Manuel Cota Villa <75243865+Keiforoid@users.noreply.github.com>
2022-01-25 20:32:53 -0800

    06_Entity_Framework_T9_Finished

---
+++ 06_AspNetCore_MVC_con_Entity_Framework/T9/GymManager.Web/GymManager.ApplicationServices/Members/MembersAppService.cs
@@ -0,0 +1,48 @@
+﻿using GymManager.Core.Members;
+using GymManager.DataAccess.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymManager.ApplicationServices.Members
+{
+    public class MembersAppService : IMemberAppService
+    {
+        private readonly IRepository<int, Member> _repository;
+
+        public MembersAppService(IRepository<int,Member> repository)
+        {
+            _repository = repository;
+        }
+
+        public virtual async Task<int> AddMemberAsync(Member member)
+        {
+            await _repository.AddAsync(member);
+
+            return member.Id;
+        }
+
+        public virtual async Task DeleteMemberAsync(int memberId)
+        {
+            await _repository.DeleteAsync(memberId);
+        }
+
+        public virtual async Task EditMemberAsync(Member member)
+        {
+            await _repository.UpdateAsync(member);
+        }
+
+        public virtual async Task<Member> GetMemberAsync(int memberId)
+        {
+            return await _repository.GetAsync(memberId);
+        }
+
+        public async Task<List<Member>> GetMembersAsync()
+        {
+            return await _repository.GetAll().ToListAsync();
+        }
+    }
+}
---
+++ 06_AspNetCore_MVC_con_Entity_Framework/T9/GymManager.Web/GymManager.Web/Controllers/MembersController.cs
@@ -0,0 +1,103 @@
+﻿using GymManager.ApplicationServices.Members;
+using GymManager.Core.Members;
+using GymManager.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymManager.Web.Controllers
+{
+    [Authorize]
+    public class MembersController : Controller
+    {
+        private readonly IMemberAppService _membersAppService;
+        ILogger _logger;
+
+        public MembersController(IMemberAppService membersAppService, ILogger logger)
+        {
+            _membersAppService = membersAppService;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            List<Member> members = await _membersAppService.GetMembersAsync();
+
+            MemberListViewModel viewModel = new MemberListViewModel();
+
+            viewModel.NewMembersCount = 2;
+            viewModel.Members = members;
+
+            _logger.Information("Index in Members executed");
+
+            return View(viewModel);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(int memberId)
+        {
+            await _membersAppService.DeleteMemberAsync(memberId);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(int memberId)
+        {
+
+            Member member = await _membersAppService.GetMemberAsync(memberId);
+
+            MemberViewModel viewModel = new MemberViewModel
+            {
+                AllowNewsletter = member.AllowNewsLetter,
+                BirthDay = member.BirthDay,
+                CityId = member.City.Id,
+                Email = member.Email,
+                Id = member.Id,
+                LastName = member.LastName,
+                Name = member.Name
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(MemberViewModel viewModel)
+        {
+            Member member = new Member
+            {
+                Name = viewModel.Name,
+                LastName = viewModel.LastName,
+                Email = viewModel.Email,
+                City = new City
+                {
+                    Id = viewModel.CityId
+                },
+                BirthDay = viewModel.BirthDay,
+                AllowNewsLetter = viewModel.AllowNewsletter
+            };
+
+            await _membersAppService.AddMemberAsync(member);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Member member)
+        {
+            await _membersAppService.EditMemberAsync(member);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+    }
+}
