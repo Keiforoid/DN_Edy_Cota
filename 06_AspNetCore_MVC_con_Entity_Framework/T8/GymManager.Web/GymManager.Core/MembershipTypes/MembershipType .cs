708580a02101084f17ce5fe8f90329748180c9fa
Edy Jesús Manuel Cota Villa <75243865+Keiforoid@users.noreply.github.com>
2022-01-25 18:36:07 -0800

    06_Entity_Framework_T8_Finished

---
+++ 06_AspNetCore_MVC_con_Entity_Framework/T8/GymManager.Web/GymManager.Core/MembershipTypes/MembershipType .cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManager.Core.MembershipTypes
+{
+    public class MembershipType
+    {
+        public int Id { get; set; }
+
+        [StringLength(100)]
+        [Required(ErrorMessage = "You must enter the name of the membership type.")]
+        public string Name { get; set; }
+
+        //[DisplayFormat(DataFormatString = "{0:C}")]
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString = "{0:C}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "You must enter a price for the membership.")]
+        public double Cost { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid duration.")]
+        [Required(ErrorMessage = "You must enter the duration of the memebership.")]
+        public int Duration { get; set; }
+    }
+}
---
+++ 06_AspNetCore_MVC_con_Entity_Framework/T8/GymManager.Web/GymManager.Web/Controllers/UsersController.cs
@@ -0,0 +1,103 @@
+﻿using GymManager.DataAccess;
+using GymManager.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymManager.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly GymManagerContext _context;
+
+        public UsersController(UserManager<IdentityUser> userManager, GymManagerContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+
+        [Authorize]
+        public async Task<ActionResult> Index()
+        {
+            var user = await _userManager.Users.ToListAsync();
+            return View(user);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(UserViewModel viewModel)
+        {
+            var result = await _userManager.CreateAsync(new IdentityUser
+            {
+                Email = viewModel.UserName,
+                EmailConfirmed = true,
+                UserName =viewModel.UserName,
+                PhoneNumber = viewModel.PhoneNumber
+            },viewModel.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+                return View();
+            }
+        }
+
+        public async Task<IActionResult> Edit(String userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+
+            UserViewModel viewModel = new UserViewModel
+            {
+                PhoneNumber = user.PhoneNumber,
+                UserName = user.UserName,
+                Id = user.Id
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(UserViewModel userViewModel)
+        {
+            var user = await _userManager.FindByIdAsync(userViewModel.Id);
+
+            user.PhoneNumber = userViewModel.PhoneNumber;
+
+            string hashedNewPassword = _userManager.PasswordHasher.HashPassword(user, userViewModel.Password);
+
+            UserStore<IdentityUser> store = new UserStore<IdentityUser>(_context);
+
+            await store.SetPasswordHashAsync(user, hashedNewPassword);
+            await store.UpdateAsync(user);
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+
+            await _userManager.DeleteAsync(user);
+            return RedirectToAction("Index");
+        }
+    }
+}
